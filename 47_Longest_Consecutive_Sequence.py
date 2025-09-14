54c8b0b08643d162576e888b1696413e8c8c21a3
SMejia115 <s.mejia6@utp.edu.co>
2025-09-13 23:59:15 -0500

    47_Longest_Consecutive_Sequence

---
+++ 47_Longest_Consecutive_Sequence.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        num_set = set(nums)
+        longest = 0
+
+        for num in num_set:
+            # only start if num is the beginning of a sequence
+            if num - 1 not in num_set:
+                current = num
+                length = 1
+
+                while current + 1 in num_set:
+                    current += 1
+                    length += 1
+
+                longest = max(longest, length)
+
+        return longest
+        
